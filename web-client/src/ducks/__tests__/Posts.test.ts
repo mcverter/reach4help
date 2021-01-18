3bcb2b920a3bb020126d80bfe83accbc3cf3bdc2
mcverter <mitchell.verter@gmail.com>
2021-01-18 09:55:32 -0500

    starting async and firebase

--- web-client/src/ducks/__tests__/Posts.test.ts
+++ web-client/src/ducks/__tests__/Posts.test.ts
@@ -1,5 +1,7 @@
-// eslint-disable-next-line no-unused-vars
-import * as unused from 'src/ducks/profile/actions';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 const types = {
   CREATE_PUBLIC_OFFER: 'CREATE_PUBLIC_OFFER',
@@ -10,6 +12,10 @@ const types = {
     'CREATE_PRIVATE_OFFER_TO_PUBLIC_REQEST',
 };
 
+/**
+ * These should be async
+ * https://redux.js.org/recipes/writing-tests#async-action-creators
+ */
 describe('actions', () => {
   it('should have action to CREATE_PUBLIC_OFFER', () => {
     const text = 'Finish docs';
@@ -42,5 +48,56 @@ describe('actions', () => {
       text,
     };
     expect(expectedAction).toEqual(expectedAction);
+  });
+});
+
+/*******
+ * https://www.robinwieruch.de/firebase-test
+ ********/
+
+jest.mock('./firebase', () => {
+  const set = jest.fn();
+
+  return {
+    database: jest.fn(() => ({
+      ref: jest.fn(() => ({
+        push: jest.fn(() => ({
+          set,
+        })),
+      })),
+    })),
+  };
+});
+
+describe('createFreeCourse', () => {
+  it('creates a course', async () => {
+    const set = firebaseAdmin
+      .database()
+      .ref()
+      .push().set;
+
+    const result = createCourse(
+      '1',
+      'THE_ROAD_TO_GRAPHQL',
+      'STUDENT',
+      0,
+      'FREE',
+    );
+
+    await expect(result).resolves.toEqual(true);
+
+    expect(set).toHaveBeenCalledTimes(1);
+
+    expect(set).toHaveBeenCalledWith({
+      courseId: 'THE_ROAD_TO_GRAPHQL',
+      packageId: 'STUDENT',
+      invoice: {
+        createdAt: 'TIMESTAMP',
+        amount: 0,
+        licensesCount: 1,
+        currency: 'USD',
+        paymentType: 'FREE',
+      },
+    });
   });
 });
